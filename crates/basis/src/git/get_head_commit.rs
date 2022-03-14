dc5014e29c257c64f6ea95571a3c306e2298d8e6
sabertazimi <sabertazimi@gmail.com>
2022-03-14 15:27:24 +0800

    style(rust-basis): format code

--- crates/basis/src/git/get_head_commit.rs
+++ crates/basis/src/git/get_head_commit.rs
@@ -1,13 +1,13 @@
-use git2::{Commit, ObjectType, Repository};
+use git2::{Commit, Error, ObjectType, Repository};
 
 pub trait GetHeadCommit {
-    fn get_head_commit(&self) -> Result<Commit<'_>, git2::Error>;
+    fn get_head_commit(&self) -> Result<Commit, Error>;
 }
 
 impl GetHeadCommit for Repository {
-    fn get_head_commit(&self) -> Result<Commit<'_>, git2::Error> {
+    fn get_head_commit(&self) -> Result<Commit, Error> {
         let obj = self.head()?.resolve()?.peel(ObjectType::Commit)?;
         obj.into_commit()
-            .map_err(|_| git2::Error::from_str("Couldn't find commit!"))
+            .map_err(|_| Error::from_str("Couldn't find commit!"))
     }
 }
